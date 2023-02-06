478326eff4ffbdb0996c22dd965ea15cbec95a2f
Felipe Kinoshita <kinofhek@gmail.com>
2023-02-06 01:47:59 -0300

    Add help menu and version information

--- main.c
+++ main.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+
+void help(void);
+void version(void);
 
 void print_stdin(void);
 void print_file(char *file_path);
 
 int main(int argc, char **argv)
 {
+	int help_flag = 0;
+	int version_flag = 0;
+
+	int c;
+
+	while ((c = getopt(argc, argv, "hv")) != -1) {
+		switch (c) {
+		case 'h':
+			help_flag = 1;
+			break;
+		case 'v':
+			version_flag = 1;
+			break;
+		default:
+			help_flag = 1;
+			break;
+		}
+	}
+
+	if (help_flag) {
+		help();
+	}
+
+	if (version_flag) {
+		version();
+	}
+
 	if (argc < 2) {
 		print_stdin();
 	}
@@ -42,4 +73,20 @@ void print_file(char *file_path)
 	}
 
 	fclose(file);
+}
+
+void help(void)
+{
+	printf("Usage: kit [OPTION]\n");
+	printf("Outputs files to stardard output.\n");
+	printf("\n  -v\t\t\toutput version information and exit.\n");
+	printf("  -h\t\t\tdisplay this help and exit.\n");
+	exit(EXIT_SUCCESS);
+}
+
+void version(void)
+{
+	printf("kit 0.1\n");
+	printf("\nWritten by Felipe Kinoshita.\n");
+	exit(EXIT_SUCCESS);
 }
